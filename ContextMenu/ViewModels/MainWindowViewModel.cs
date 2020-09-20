255589d46b4f3ca27b059ba6bd21872ed48729b2
Kir-Antipov <kp_antipov@mail.ru>
2020-09-20 22:50:22 +0300

    Added example to git

---
+++ ContextMenu/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace ContextMenu.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public ObservableCollection<string> Items { get; } = new ObservableCollection<string>();
+
+        public void Add() => Items.Add(DateTime.Now.ToString("O"));
+
+        public void Remove()
+        {
+            if (Items.Count > 0)
+                Items.RemoveAt(0);
+        }
+
+        public void MenuItemClicked() { }
+    }
+}
